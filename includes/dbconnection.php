95e316884f59064234cc22f9aead3c63ed0de3c7
Izzopedrosa <pedrosaizzoooo@gmail.com>
2024-06-25 08:44:31 +0800

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,6 +1,6 @@
 <?php
 // connect to the database
-$conn = mysqli_connect("localhost", "root", "", "system_data");
+$conn = mysqli_connect("database-1.c3gskywgqkaj.ap-southeast-2.rds.amazonaws.com", "admin", "Unlock-RDS", "system_data");
 // Check for connection errors
 if (mysqli_connect_errno()) {
   die("Connection Fail" . mysqli_connect_error());
